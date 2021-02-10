1084d4cbfb41736e74b11693a21fc5ef391e6fd7
Remi Schwartz <schwartz.remi@gmail.com>
2021-02-10 09:28:35 +0100

    bg alternance + button hover

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,15 +1,34 @@
 const showBanner = () => {
   const paintingThumbnails = document.querySelectorAll('.painting-thumbnail');
+  const banner = document.querySelector('.banner');
+  // const temp = banner.getBoundingClientRect();
+  // const bannerCoords = banner.getBoundingClientRect();
+  // const bannerWidth = bannerCoords.width;
+  const bannerHeight = 400;
+  const windowWidth = window.innerWidth;
+    // console.log(temp);
+    console.log(bannerHeight);
+    console.log(windowWidth);
+
+
   paintingThumbnails.forEach( thumbnail => {
-    const factor = Math.random();
-    thumbnail.style.zIndex = `${Math.floor(factor * 100)}`;
-    console.log(factor);
+    const factor1 = Math.random();
+    thumbnail.style.top = `${Math.random() * 0.7 * bannerHeight}px`;
+    thumbnail.style.left = `${Math.random() * 0.6 * windowWidth + 0.1 * windowWidth}px`;
+    thumbnail.style.zIndex = `${Math.floor(factor1 * 100)}`;
+    thumbnail.style.transform = `rotate(${factor1*60 -30}deg)`;
+
     window.setTimeout( () => {
       thumbnail.classList.add('show');
-      thumbnail.style.transform = `rotate(${factor*60 -30}deg) scale(0.95)`;
-
-      }, factor * 1000)
+      thumbnail.style.transform = `rotate(${factor1*60 -30}deg) scale(0.95)`;
+      }, factor1 * 1000)
   });
+
+  const title = document.querySelector('.main-title');
+  window.setTimeout( () => {
+    title.style.transition = "opacity 0.5s ease-out";
+    title.style.opacity = 1;
+  }, 1000);
 
 }
 
